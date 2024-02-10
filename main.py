c6465027da06a53c25ced20598a2192897796122
Mahak Agrawal <132470319+Mahak-Agrawal-304@users.noreply.github.com>
2024-02-11 01:03:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,12 @@ import tkinter as tk
 from tkinter import *
 from tkinter import ttk
 from tkinter import messagebox
+from tkinter import Tk, filedialog
+
+#for text detection
+import pandas as pd
+import easyocr
+import cv2
 
 #import googletrans library
 from googletrans import Translator,LANGUAGES
@@ -15,12 +21,11 @@ from langdetect import detect
 from gtts import gTTS
 import os
 
-
 #creating object window 
 root = Tk()
-root.geometry("500x650")#Geometry
+root.geometry("500x700")#Geometry
 root.title("Translator")#Title
-root.config(bg='#fefbfa')#config color 
+root.config(bg='#fefbfa')#config meand color and all
 
 
 #Function for translation
@@ -28,24 +33,29 @@ def get_data():
     comb_sou_option=comb_sou.get()
     comb_des_option=comb_des.get()
     txt = sou_txt.get(1.0,tk.END)
-    trans = Translator()
-    trans1 = trans.translate(text=txt,src=comb_sou_option,dest=comb_des_option)
+    try:
+        trans = Translator()
+        trans1 = trans.translate(text=txt,src=comb_sou_option,dest=comb_des_option)
     
-    des_txt.delete(1.0, tk.END)
-    des_txt.insert(tk.END,trans1.text)
+        des_txt.delete(1.0, tk.END)
+        des_txt.insert(tk.END,trans1.text)
+        return None
+    except Exception as e:
+        messagebox.showerror("Error","Please enter text to be translated")
+        return None
     
 
 #Function for clearing all values       
 def clearall():
     sou_txt.delete(1.0,tk.END)
     des_txt.delete(1.0,tk.END)
+    upld("")
 
-#Function for recording speech
+
+#Functionto record audio
 def recognize_speech():
     recognizer = sr.Recognizer()
     with sr.Microphone() as source:
-        text="Speak something..."
-        sou_txt.insert(tk.END,text)
         audio = recognizer.listen(source)
 
     try:
@@ -61,8 +71,7 @@ def recognize_speech():
         messagebox.error("Error",f"Could not request results from Google Speech Recognition service; {e}")
         return ""
 
-
-#Function for playing translated text
+#Function to convert text into speech
 def text_to_speech():
     text=des_txt.get(1.0,tk.END)
     comb_des_option=comb_des.get()
@@ -72,21 +81,63 @@ def text_to_speech():
     os.system("start output.mp3")
 
 
+#Function to upload file
+def select_image_file():
+    root = Tk()
+    root.withdraw()  # Hide the root window
+
+    # Prompt the user to select an image file using a file dialogue
+    file_path = filedialog.askopenfilename(filetypes=[("Image Files", "*.jpg;*.jpeg;*.png;*.bmp")])
+    image_file_path = file_path
+    img1=image_file_path
+    filename=os.path.basename(img1)
+    upld(filename)
+
+    img = cv2.imread(img1)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    #noise = cv2.medianBlur(gray,3)
+    blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+    thresh = cv2.adaptiveThreshold(blurred, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 11, 4)
+
+    #thresh = cv2.threshold(noise, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+
+    reader = easyocr.Reader(['en'])
+    result = reader.readtext(img,paragraph=False)
+    text_lines = [entry[1] for entry in result[0::]]  # Exclude the last line
+    text = ' '.join(text_lines)
+    df = pd.DataFrame({"":text_lines})
+
+    sou_txt.delete(1.0,tk.END)
+    sou_txt.insert(tk.END,df.to_string(index=False,header=False))
+    
+
 #Creating a label text and placing it in object window
 lbl_txt = Label(root,text="Translator",font=("Times New Roman",30),bg='#fefbfa')
 lbl_txt.place(x=100,y=15,height=40,width=300)
 
 
 #Creating label Source text
-lbl_txt = Label(root,text="Source Text",font=("Times New Roman",16),bg='#fefbfa' )
+lbl_txt = Label(root,text="Source Text",font=("Times New roman",16),bg='#fefbfa' )
 lbl_txt.place(x=0,y=80,height=30,width=130)
-sou_txt = Text(root,font=("Times New Roman",14))
-sou_txt.place(x=10,y=120,height=200,width=480)
+sou_txt = Text(root,font=("Times New roman",14))
+sou_txt.place(x=10,y=145,height=200,width=480)
 
 
 #creating list for languages
-list_txt = list(LANGUAGES.values()) 
-
+list_txt = ('Bengali',
+            'English',
+            'Gujarati',
+            'Hindi',
+            'Kannada',
+            'Malayalam',
+            'Marathi',
+            'Nepali',
+            'Odia',
+            'Punjabi',
+            'Sindhi',
+            'Tamil',
+            'Telugu',
+            'Urdu')
 
 #Creating combobox for from language
 comb_sou= ttk.Combobox(root,values = list_txt)
@@ -96,38 +147,49 @@ comb_sou.set("English")
 
 #Creating combobox for to language
 comb_des= ttk.Combobox(root,values = list_txt)
-comb_des.place(x=200,y =400,height = 30,width= 100)
+comb_des.place(x=200,y =430,height = 30,width= 100)
 comb_des.set("Hindi")
 
 
 #Creating label for output 
-lbl_txt = Label(root,text="Translated Text",font=("Times New Roman",16),bg='#fefbfa' )
-lbl_txt.place(x=0,y=400,height=30,width=155)
-des_txt = Text(root,font=("Times New Roman",14),wrap=WORD)
-des_txt.place(x=10,y=440,height=200,width=480)
+lbl_txt = Label(root,text="Translated Text",font=("Times New roman",16),bg='#fefbfa' )
+lbl_txt.place(x=0,y=430,height=30,width=155)
+des_txt = Text(root,font=("Times New roman",14),wrap=WORD)
+des_txt.place(x=10,y=470,height=200,width=480)
+
+def upld(image):
+    lbl_txt = Label(root,text=f"Image Uploaded: {image}",font=("Times New roman",12),bg='#fefbfa' )
+    lbl_txt.place(x=300,y=110,height=30,width=200)
 
 
 #Button for translation
 Btn1=Button(root,text='Translate',relief=RAISED,command=get_data, bg="white")
-Btn1.place(x=120,y=340,height = 30,width = 100)
+Btn1.place(x=120,y=370,height = 30,width = 100)
 
 
 #Button for voice translation
 img1_path="C:/Users/HP/HackAI/voice.png"
 img1=PhotoImage(file=img1_path)
 voicebtn= Button(root,image=img1,command=recognize_speech)
-voicebtn.place(x=450,y=80,height=30,width=30)
+voicebtn.place(x=400,y=80,height=30,width=30)
 
 
 #Button for playing translated text
 img2_path="C:/Users/HP/HackAI/play.png"
 img2=PhotoImage(file=img2_path)
 playbtn= Button(root,image=img2,command=text_to_speech)
-playbtn.place(x=450,y=400,height=30,width=30)
+playbtn.place(x=450,y=430,height=30,width=30)
+
+
+#Button for uploading image
+img3_path="C:/Users/HP/HackAI/upload.png"
+img3=PhotoImage(file=img3_path)
+upldbtn= Button(root,image=img3, command=select_image_file)
+upldbtn.place(x=450,y=80,height=30,width=30)
 
 
 #Button for Clear all
 Btn2=Button(root,text='Clear all',relief=RAISED,command=clearall,bg="white")
-Btn2.place(x=280,y=340,height = 30,width = 100)
+Btn2.place(x=280,y=370,height = 30,width = 100)
 
 root.mainloop()
